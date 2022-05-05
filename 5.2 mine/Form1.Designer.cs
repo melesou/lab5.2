4bc64c1b6e2c1b9b37e6030905f409fb74efa78d
melesou <melesou@mail.ru>
2022-05-05 16:10:16 +0500

    Добавьте файлы проекта.

---
+++ 5.2 mine/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _5._2_mine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 205);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Температура в выбранный день";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 242);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 5.2 mine/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics.Eventing.Reader;
+
+namespace _5._2_mine
+{
+    public partial class Form1 : Form
+    {
+        string currDay = "";
+        string currMonth = "";
+        string currYear = "";
+        public Form1()
+        {
+            InitializeComponent();
+            dataProcessing();
+        }
+
+        public void dataProcessing()
+        {
+            string line;
+            string word = "";
+            int count = 0;
+            int day = 0, year = 0, month = 0;
+            FileStream fs1 = new FileStream("Календарь температур.txt", FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(fs1);
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '.' || line[i] == ':' || i + 1 == line.Length)
+                    {
+                        if (i + 1 == line.Length)
+                            word += line[i];
+                        if (count == 0)
+                            day = int.Parse(word);
+                        if (count == 1)
+                            month = int.Parse(word);
+                        if (count == 2)
+                            year = int.Parse(word);
+                        word = "";
+                        count++;
+                        if (count > 3)
+                            count = 0;
+                    }
+                    else
+                        word += line[i];
+                }
+                monthCalendar1.AddBoldedDate(new DateTime(year, month, day));
+            }
+            sr.Close();
+            fs1.Close();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            currDay = e.Start.Day.ToString();
+            currMonth = e.Start.Month.ToString();
+            currYear = e.Start.Year.ToString();
+            string line;
+            string word = "";
+            int count = 0;
+            int day = 0, year = 0, month = 0;
+            int temp = 0;
+            FileStream fs1 = new FileStream("Календарь температур.txt", FileMode.OpenOrCreate);
+            StreamReader sr = new StreamReader(fs1);
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '.' || line[i] == ':' || i + 1 == line.Length)
+                    {
+                        if (i + 1 == line.Length)
+                            word += line[i];
+                        if (count == 0)
+                            day = int.Parse(word);
+                        if (count == 1)
+                            month = int.Parse(word);
+                        if (count == 2)
+                            year = int.Parse(word);
+                        if (count == 3)
+                            temp = int.Parse(word);
+                        word = "";
+                        count++;
+                        if (count > 3)
+                            count = 0;
+                    }
+                    else
+                        word += line[i];
+                }
+
+                if (int.Parse(e.Start.Day.ToString()) == day && int.Parse(e.Start.Month.ToString()) == month && int.Parse(e.Start.Year.ToString()) == year)
+                {
+                    label1.Text = "Температура:" + temp;
+                    break;
+                }
+                else
+                    label1.Text = "Температура: не указана";
+
+            }
+            sr.Close();
+            fs1.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FileStream fs = new FileStream("Календарь температур.txt", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            if (currDay != "" && currMonth != "" && currYear != "" && textBox1.Text != "")
+                sw.WriteLine(currDay + "." + currMonth + "." + currYear + ":" + textBox1.Text);
+            else
+                MessageBox.Show("Введите дату и температуру");
+            sw.Close();
+            fs.Close();
+            dataProcessing();
+            monthCalendar1.Refresh();
+        }
+    }
+}
